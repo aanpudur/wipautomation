fe75b6ceff19e16da17dac9353d18173db75c6e2
Aan Vazhapilly <aanpudur@gmail.com>
2017-02-13 08:38:17 -0800

    Updated circle program with custom constructor and nonstatic variable

--- src/com/wip/training/excercise5/Circle.java
+++ src/com/wip/training/excercise5/Circle.java
@@ -7,19 +7,14 @@ package com.wip.training.excercise5;
 public class Circle {
   int radius;
   double pie = 3.14;
-
-//area formulae is π * 2 * 2
-  public double area(int rad) {
-    return pie * rad * rad;
-  }
   
-  //perimeter formulae  2 * π * R
-  public double peremeter(int rad){
-    
-    return 2 * pie * rad;
-    
+  public Circle(int a){
+   radius = a;
     
   }
+
+
+  
 
 
 
